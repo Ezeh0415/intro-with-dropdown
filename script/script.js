4371b937a4ef4d2dfcdf4ddd65c0f5beec87ec73
Ezeh Godwin <ezehgodwin3942@gmail.com>
2023-06-12 06:13:19 -0700

    frontend mentor project

---
+++ script/script.js
@@ -0,0 +1,32 @@
+$("#feu").on("click",function(){
+     $("#futo").toggle(1000);
+});
+
+$("#comp").on("click",function(){
+      $("#futa").toggle(1000);
+});
+
+
+$("#sp").on("click",function(){
+     $("#borddiv").toggle(1000);
+     $("#h1flexdiv h1").toggle(1000);
+     $("#bodydiv p").toggle(1000);
+     $("button").toggle(1000);
+     $("#logo-img-div img").toggle(1000);
+});
+
+$("#feuu").on("click",function(){
+     $("#futoo").toggle(1000);
+});
+
+$("#compp").on("click",function(){
+     $("#futaa").toggle(1000);
+});
+
+
+
+
+
+
+
+
